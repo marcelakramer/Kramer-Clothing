52cd95dbe88519b0238d698163067db2e85c5ec1
Marcela Kramer <marcelabarretokramer10@gmail.com>
2023-10-28 21:24:24 -0300

    insert plans and kits in db.json

--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -1,9 +1,14 @@
 export class User {
   constructor(
+    private _id: number,
     private _name: string,
     private _email: string,
     private _password: string
   ) {}
+
+  get id(): number {
+    return this._id;
+  }
 
   get name(): string {
     return this._name;
