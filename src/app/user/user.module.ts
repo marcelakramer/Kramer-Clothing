8643098faf2dba255a14f670c54d6f48b70a3ba8
Marcela Kramer <marcelabarretokramer10@gmail.com>
2023-11-05 20:31:46 -0300

    create sign-up component and routes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent},  
+  { path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   declarations: [
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
@@ -23,7 +30,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatButtonModule
   ],
   exports: [
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ]
 })
 export class UserModule { }
