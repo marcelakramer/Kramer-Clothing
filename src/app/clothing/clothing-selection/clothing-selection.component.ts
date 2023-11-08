b0ffdc014d631622c42859910d707b409f6ce685
Marcela Kramer <marcelabarretokramer10@gmail.com>
2023-11-07 21:04:53 -0300

    add selection funcionality

--- src/app/clothing/clothing-selection/clothing-selection.component.ts
+++ src/app/clothing/clothing-selection/clothing-selection.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Clothing } from 'src/app/shared/model/clothing';
+import { Plan } from 'src/app/shared/model/plan';
+import { Kit } from 'src/app/shared/model/kit';
+import { ClothingService } from 'src/app/shared/services/clothing.service';
 @Component({
   selector: 'app-clothing-selection',
   templateUrl: './clothing-selection.component.html',
   styleUrls: ['./clothing-selection.component.scss']
 })
-export class ClothingSelectionComponent {
+export class ClothingSelectionComponent implements OnInit{
+  clothes: Clothing[] = [];
+  selectedClothes: Clothing[] = [];
+  clothesRemaining: number = 5;
+  //plan: Plan;
+  //kit: Kit;
+
+  constructor(private clothingService: ClothingService) {}
+
+  ngOnInit(): void {
+      this.clothingService.getAll().subscribe(
+        response => {
+          response.map((clothing) => {
+            this.clothes.push(
+              new Clothing(
+                clothing.description,
+                clothing.brand,
+                clothing.size,
+                clothing.color,
+                clothing.material
+              )
+            ); 
+          });
+        }
+      )
+  }
+
+  toggleSelected(clothing: Clothing): void {
+    if (this.isSelected(clothing)) {
+      this.selectedClothes = this.selectedClothes.filter((c) => c !== clothing);
+    } else if (this.clothesRemaining === 0) {
+      console.log('Máximo atingido.');
+    } else {
+      this.selectedClothes.push(clothing);
+    }
+    this.calcClothesRemaining()
+    console.log(this.selectedClothes);
+  }
+
+  isSelected(clothing: Clothing): boolean {
+    return this.selectedClothes.includes(clothing);
+  }
+
+  calcClothesRemaining(): void {
+    this.clothesRemaining = 5 - this.selectedClothes.length;
+  }
+
+  canContinue(): boolean {
+    return this.clothesRemaining === 0;
+  }
+
 }
