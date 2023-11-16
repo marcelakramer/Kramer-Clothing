7e3690492ebeeb4bb8c37b3d687902466bb7b9b9
Matheus Pereira de Sousa <20221370007@ad.ifpb.edu.br>
2023-11-16 13:39:47 -0300

    adicionando redirecionamento

--- src/app/layout/initial-section/initial-section.component.ts
+++ src/app/layout/initial-section/initial-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-initial-section',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./initial-section.component.scss']
 })
 export class InitialSectionComponent {
+  constructor(private router: Router) {
 
+  }
+
+  goToSignUp() {
+    console.log("uepa");
+    
+    this.router.navigate(['/sign-up'])
+  }
 }
