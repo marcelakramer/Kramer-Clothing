061dd27f1279e2dbf237e8aa5900ec0d9def8bc0
Marcela Kramer <marcelabarretokramer10@gmail.com>
2023-12-20 11:21:48 -0300

    update user signup info

--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -3,6 +3,8 @@ export class User {
   constructor(
     public id: string,
     public name: string,
+    public cpf: string,
+    public phoneNumber: string,
     public email: string,
     public password: string
   ) { }
